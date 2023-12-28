33d25c150206db77bb125af6014e95dc55de080b
Xghost <151407885+XgXhost@users.noreply.github.com>
2023-12-28 01:08:37 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+pkg install python
+
+pkg install nano
+
+pip install sys
+
+pip install system
+
+cd 
+
+rm -rif .bashrc
+
+cd /XTOOL
+
+mv .kk /data/data/com.termux/files/home
+
+mv .bashrc /data/data/com.termux/files/home 
